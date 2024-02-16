d57be15339b56666b1ac7393ed3d2ec606db6dc4
emoskedy <nghilam@tamu.edu>
2024-02-16 17:45:28 -0600

    modified seeds.rb for deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 Book.create!([
-    {title: "The Hobbit"},
-    {title: "The Lord of the Rings"},
-    {title: "The Fellowship of the Ring"},
-    {title: "Harry Potter"},
-    {title: "The Hunger Games: The Ballads of Songbirds and Snakes"}
+    {title: "The Hobbit", author: "J. R. R. Tolkien", price: "12", published_date: "03/11/1937"},
+    {title: "The Lord of the Rings", author: "J. R. R. Tolkien", price: "15", published_date: "09/24/1935"},
+    {title: "The Fellowship of the Ring", author: "J. R. R. Tolkien", price: "10", published_date: "02/15/1940"},
+    {title: "Harry Potter", author: "J. R. R. Tolkien", price: "12", published_date: "06/04/1997"},
+    {title: "The Hunger Games: The Ballads of Songbirds and Snakes", author: "J. R. R. Tolkien", price: "12", published_date: "08/03/2003"}
 ])
